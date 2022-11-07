4d60b67cec3767ff504b9c3760d9cd534e864526
LazzzyBlazer <lazzzyblazer@gmail.com>
2022-11-07 14:52:02 +0300

    v0.0.1
    -ball
    -dots mechanic
    -basic scene settings

---
+++ dunkTestPrj/Assets/Basket.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    public GameObject ballPrf;
+    public float launchForce;
+    public Transform shotPoint;
+
+    public GameObject point;
+    GameObject[] points;
+    public int numbersOfPoints;
+    public float spaceBetweenPoints;
+    Vector2 direction;
+
+    private void Start() 
+    {
+        points = new GameObject[numbersOfPoints];
+        for(int i = 0; i < numbersOfPoints; i++)
+        {
+            points[i] = Instantiate(point, shotPoint.position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        Vector2 ballPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePosition - ballPosition;
+        transform.right = direction;
+    
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+        for(int i = 0; i < numbersOfPoints; i++)
+        {
+            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newBall = Instantiate(ballPrf, shotPoint.position, shotPoint.rotation);
+        newBall.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+    }
+    
+    Vector2 PointPosition(float t) {
+        Vector2 position = (Vector2)shotPoint.position + (direction.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t * t);
+        return position;
+    }
+}
